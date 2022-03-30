e62d7fc7f35d7a5ab788162233414b07574dadf9
jisobkim <jisobkim@sbs.co.kr>
2022-03-30 11:38:03 +0900

    p10k setting & install

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+echo "alias ll='ls -lGaf'" >> ~/.bash_profile
+source ~/.bash_profile
